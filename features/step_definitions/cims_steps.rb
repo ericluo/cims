faf81aee51664bf171c6905cc8c178663ca382f0
Eric <eric@Eric.(none)>
2009-04-03 16:20:10 +0800

    add all

--- features/step_definitions/cims_steps.rb
+++ features/step_definitions/cims_steps.rb
@@ -1,5 +1,3 @@
 Given /^a purchase existed by "(.*)" on "(.*)" from "(.*)":$/ do |buyer, buy_date, supplier, items|
-  puts buyer, supplier, items
-  Purchase.create!(:buyer => buyer, :supplier => supplier, :buyer_date => buy_date)
-  puts buy_date
+  Purchase.create!(:buyer => buyer, :supplier => supplier, :buy_date => buy_date)
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Product do
+  before(:each) do
+    @valid_attributes = {
+      :name => "value for name",
+      :brand => "value for brand",
+      :mode => "value for mode",
+      :category_id => 1,
+      :detail => "value for detail"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Product.create!(@valid_attributes)
+  end
+end
