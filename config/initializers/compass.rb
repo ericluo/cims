5e6563c2a8f297e769f747cb3d12db073dc619d1
eric <eric@home.(none)>
2009-08-18 00:23:08 +0800

    enable the compass framework

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,11 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+# Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+
+Compass.configuration do |config|
+  config.project_path = RAILS_ROOT
+  config.sass_dir = "app/stylesheets"
+  config.css_dir = "public/stylesheets/compiled"
+end
 Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
